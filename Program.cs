c3a7188d888c0d29efe0251bcf5fc6d3b2bc8f1b
ethanryder7 <122943734+ethanryder7@users.noreply.github.com>
2023-01-23 23:33:10 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "x = 2.5, y = 3.3";
+        Console.WriteLine("The value of Z is 34.9");
+    }
+}
